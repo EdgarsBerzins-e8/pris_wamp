2a449f897d9b4d31eb06834e7d539c7444efcf46
EdgarsBerzins-e8 <edgars.berzins@celvedis.lv>
2022-05-03 09:09:55 +0300

    Add comments into class persons

--- classes/persons.php
+++ classes/persons.php
@@ -4,22 +4,27 @@ class Persons extends Database
 {
   protected $var, $data = [];
 
+  // get all field of all persons from persons table ordered by first name
   public function getAllPersons()
   {
     return $this->get('persons', null, 'name');
   }
 
+  // get all fields of one person by id
   public function getPerson($var)
   {
     return $this->get('persons', $var);
   }
 
+  // add new person
   public function addPerson($data)
   {
     return $this->add('persons', $data);
   }
 
+  // update person by id
   public function updatePerson($data)
+  // id should be in $data['id']
   {
     return $this->set('persons', $data);
   }
